6350a899771aad16aecf70231b297b5eb584b894
ChanakyaSai05 <chanakyasai1996@gmail.com>
2022-03-12 13:52:57 +0530

    homepageadded

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
+  // const navigate = useNavigate();
   return (
-    <div>
-      Hii<Link to="/account">Readmore</Link>
+    <div className="container">
+      <div className="welcome">
+        <h1>Welcome</h1>
+        <button className="btn btn-sm m-2 btn-success">
+          <Link
+            to="/products"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Go to Products
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
